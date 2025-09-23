1d7752cb6507af92a0e11e434706b67193adaf68
mohamedmufeed <mohamedmufeed44@gmail.com>
2025-09-23 17:38:20 +0530

    feat: setup socket.io signaling server

---
+++ server/src/server.ts
@@ -0,0 +1,48 @@
+import express from "express"
+import { createServer } from "http"
+import { Server } from "socket.io"
+
+const app = express()
+const httpServer = createServer(app)
+
+
+const io = new Server(httpServer, {
+    cors: { origin: "*" }
+})
+
+// Connection
+io.on("connection", (socket) => {
+    console.log("A user connected :", socket.id)
+
+    // Join a room
+    socket.on("join-room", (roomId: string) => {
+        socket.join(roomId)
+        console.log(`${socket.id} joined room ${roomId}`)
+        socket.to(roomId).emit("user-joined", socket.id)
+    })
+
+    // Forward offer
+    socket.on("offer", (data: { offer: any, to: string }) => {
+        io.to(data.to).emit("offer", { offer: data.offer, from: socket.id })
+    })
+
+    // Forward answer
+    socket.on("answer", (data: { answer: any, to: string }) => {
+        io.to(data.to).emit("answer", { answer: data.answer, from: socket.id })
+    })
+
+    // Forward ICE candidates
+    socket.on("ice-candidate", (data: { candidate: any, to: string }) => {
+        io.to(data.to).emit("ice-candidate", { candidate: data.candidate, from: socket.id })
+    })
+
+    // Disconnect
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+    })
+
+})
+
+httpServer.listen(5001, () => {
+    console.log("Server listening on port 5001");
+})
