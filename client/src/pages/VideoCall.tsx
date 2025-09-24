4b2c8c1a43f6b3a85303a06e8be0f1b1d4a31fc3
mohamedmufeed <mohamedmufeed44@gmail.com>
2025-09-24 12:24:53 +0530

    feat: screen share feture addedd

--- client/src/pages/VideoCall.tsx
+++ client/src/pages/VideoCall.tsx
@@ -1,4 +1,4 @@
-import { Mic, Video, Phone, MessageCircle, ScreenShare, MicOff, VideoOff } from "lucide-react";
+import { Mic, Video, Phone, MessageCircle, ScreenShare, MicOff, VideoOff, ScreenShareOff } from "lucide-react";
 import Videos from "../components/Videos";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
@@ -12,13 +12,16 @@ const VideoCall = () => {
         toggleMic: (on: boolean) => void;
         toggleVideo: (on: boolean) => void;
         endCall: () => void;
+        startShareScreen:()=>void;
+        stopShareScreen:()=>void
     } | null>(null);
-    const [status,setStatus]=useState("")
+    const [status, setStatus] = useState("")
     const [micOn, setMicOn] = useState(true);
     const [camOn, setCamOn] = useState(true);
     const socket = useSocket("http://localhost:5001");
     const userId = "user-" + Math.floor(Math.random() * 1000);
     const [showChat, setShowChat] = useState(true)
+    const [startSharing, setStartSharing] = useState(false)
 
     return (
         <div className="min-h-screen bg-[#695ca8] flex flex-col lg:flex-row p-3 sm:p-8">
@@ -27,7 +30,7 @@ const VideoCall = () => {
             <div className="flex-1 flex flex-col p-6 gap-4">
 
                 {/* Meeting Heading */}
-                <MeetingHeader roomId={roomId!} status={status}/>
+                <MeetingHeader roomId={roomId!} status={status} />
 
                 {/* Video Section */}
                 <div className="flex flex-col sm:flex-row gap-6 flex-1 justify-center h-full">
@@ -42,9 +45,19 @@ const VideoCall = () => {
                             className="w-12 h-12 bg-gray-100 rounded-full flex items-center justify-center hover:bg-gray-200 transition">
                             {micOn ? <Mic className="text-gray-700" /> : <MicOff className="text-gray-700" />}
                         </button>
-                        <button className="w-12 h-12 bg-gray-100 rounded-full flex items-center justify-center hover:bg-gray-200 transition">
+
+                        {startSharing ? 
+                        <button
+                        onClick={()=>{setStartSharing(false) ; controls?.stopShareScreen()}}
+                         className="w-12 h-12 bg-gray-100 rounded-full flex items-center justify-center hover:bg-gray-200 transition">
+                            <ScreenShareOff className="text-gray-700" />
+                        </button> :
+                         <button 
+                           onClick={()=>{setStartSharing(true) ; controls?.startShareScreen()}}
+                         className="w-12 h-12 bg-gray-100 rounded-full flex items-center justify-center hover:bg-gray-200 transition">
                             <ScreenShare className="text-gray-700" />
-                        </button>
+                        </button>}
+
                         <button
                             onClick={() => controls?.endCall()}
                             className="w-14 h-14 bg-red-500 rounded-full flex items-center justify-center hover:bg-red-600 transition">
